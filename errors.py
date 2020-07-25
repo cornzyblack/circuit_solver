1ab35d4307956d97ff0f60fea3c8ba091d60a3c6
Adah Eni <eniwokepdm@gmail.com>
2020-07-25 22:13:57 +0100

    feat: add new Error messages

--- errors.py
+++ errors.py
@@ -1,6 +1,7 @@
 class BaseError(Exception):
     pass
 
+
 class NotALoopError(BaseError):
     """Exception raised when the connected components do not constitute a valid loop.
     A valid Loop is one starts from a source and ends back at that source. In general,
@@ -16,3 +17,35 @@ class NotALoopError(BaseError):
 
     def __str__(self):
         return f"This is not a Loop"
+
+
+class NotInSeries(BaseError):
+    """Exception raised when the connected components are not in Series.
+    """
+
+    def __init__(self, resistor_a, resistor_b):
+        self.resistor_a = resistor_a
+        self.resistor_b = resistor_b
+
+    def __str__(self):
+        return f"The Resistors {self.resistor_a}, {self.resistor_b} are not in Series"
+
+
+class NotInParallel(BaseError):
+    """Exception raised when the connected components are not in Parallel.
+    """
+
+    def __init__(self, resistor_a, resistor_b):
+        self.resistor_a = resistor_a
+        self.resistor_b = resistor_b
+
+    def __str__(self):
+        return f"The Resistors {self.resistor_a}, {self.resistor_b} are not in Parallel"
+
+
+class SameNodeError(BaseError):
+    """Exception raised when the .
+    """
+
+    def __str__(self):
+        return f"The Nodes cannot be the same (start_node != end_node)"
